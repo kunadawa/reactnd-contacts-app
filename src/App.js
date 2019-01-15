b68b91d0b4ae37dcafbd82eaeca9b5db65a50c94
Eric Njogu <kunadawa@gmail.com>
2019-01-15 12:23:02 +0300

    add state to 'App' component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react';
 import ContactList from './contact-list';
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-];
-
 class App extends Component {
+    state = {
+        contacts :
+            [{
+                "id": "karen",
+                "name": "Karen Isgrigg",
+                "handle": "karen_isgrigg",
+                "avatarURL": "http://localhost:5001/karen.jpg"
+            },
+            {
+                "id": "richard",
+                "name": "Richard Kalehoff",
+                "handle": "richardkalehoff",
+                "avatarURL": "http://localhost:5001/richard.jpg"
+            },
+            {
+                "id": "tyler",
+                "name": "Tyler McGinnis",
+                "handle": "tylermcginnis",
+                "avatarURL": "http://localhost:5001/tyler.jpg"
+            }
+        ]
+    }
   render() {
-    return <div><ContactList contacts={contacts}/></div>
+    return <div><ContactList contacts={this.state.contacts}/></div>
   }
 }
 
